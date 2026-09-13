6b26ca752f85904c461529ec4281d61feca2a3db
Frank Kalengay <frankkalengay@gmail.com>
2026-09-13 23:58:47 +0200

    feat: add cart mapping configuration

--- eCommerce.Application/Mapping/MappingConfig.cs
+++ eCommerce.Application/Mapping/MappingConfig.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using eCommerce.Application.DTOs.Cart;
 using eCommerce.Application.DTOs.Category;
 using eCommerce.Application.DTOs.Identity;
 using eCommerce.Application.DTOs.Product;
 using eCommerce.Domain.Entities;
+using eCommerce.Domain.Entities.Cart;
 using eCommerce.Domain.Entities.Identity;
 
 
@@ -20,6 +22,9 @@ namespace eCommerce.Application.Mapping
 
             CreateMap<CreateUser, AppUser>();
             CreateMap<LoginUser, AppUser>();
+
+            CreateMap<PaymentMethod, GetPaymentMethod>();
+            CreateMap<CreateAchieve, Achieve>();
         }
     }
 }
