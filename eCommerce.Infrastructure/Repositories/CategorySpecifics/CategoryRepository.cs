4e1e516a6870c76c1020bdafbc5c0ed23ce04090
Frank Kalengay <frankkalengay@gmail.com>
2026-09-16 00:18:24 +0200

    feat: add category specifics repository

---
+++ eCommerce.Infrastructure/Repositories/CategorySpecifics/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Interfaces.CategorySpecifics;
+using eCommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+namespace eCommerce.Infrastructure.Repositories.CategorySpecifics
+{
+    public class CategoryRepository(AppDbContext context) : ICategory
+    {
+        public async Task<IEnumerable<Product>> GetProductsByCategory(Guid categoryId)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .Where(x => x.CategoryId == categoryId)
+                .AsNoTracking()
+                .ToListAsync();
+            return products.Count > 0 ? products : [];
+        }
+    }
+}
